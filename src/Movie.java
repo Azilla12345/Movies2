3d60c98c6eaed09b5db6a1344df7d608a8d28028
hayde <why@gmail.com>
2024-03-11 19:56:49 -0400

    Code

--- src/Movie.java
+++ src/Movie.java
@@ -1,42 +1,60 @@
+import java.util.ArrayList;
+
 public class Movie {
-    private double userRating;
-    private String cast;
     private String title;
+    private ArrayList<String> cast;
     private String director;
     private String overview;
     private int runtime;
-    public Movie (double userRating, String title, String director, String overview, int runtime, String cast) {
-        this.userRating = userRating;
-        this.title = title;
-        this.director = director;
-        this.overview = overview;
-        this.runtime = runtime;
-        this.cast = cast;
+    private double rating;
+
+    public Movie(String title, String cast, String director, String overview, int runtime, double rating) {
+        this.title=title;
+        this.cast=new ArrayList<String>();
+        this.director=director;
+        this.overview=overview;
+        this.runtime=runtime;
+        this.rating=rating;
+
+        String name = "";
+        for (int i = 0; i < cast.length(); i++) {
+            if (cast.substring(i, i + 1).equals("|")) {
+                this.cast.add(name);
+                name = "";
+            } else {
+                name += cast.substring(i,i+1);
+            }
+        }
     }
 
-    public double getUserRating() {
-        return userRating;
-    }
-
-    public String getCast () {
-        return cast;
-    }
-
-    public String getTitle () {
+    public String getTitle() {
         return title;
     }
 
-    public String getDirector () {
+    public ArrayList<String> getCast() {
+        return cast;
+    }
+
+    public String getDirector() {
         return director;
     }
 
-    public String getOverview () {
+    public String getOverview() {
         return overview;
+    }
+
+    public double getRating() {
+        return rating;
     }
 
     public int getRuntime() {
         return runtime;
     }
 
-    public 
+    public String toString() {
+        return String.format(
+                "Title: %s\nRuntime: %d minutes\nDirected by: %s\nCast: %s\nOverview: %s\nRating: %f\n",
+                title, runtime, director, cast, overview, rating
+        );
+    }
 }
--- src/MovieCollection.java
+++ src/MovieCollection.java
@@ -1,69 +1,153 @@
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
-public class MovieCollection {
-    Scanner myScanner = new Scanner(System.in);
-    Movie movie;
-    ArrayList<Movie> movies = new ArrayList<Movie>();
-    public MovieCollection() {
-        start();
-    }
+class MovieCollection {
     public void start() {
+        ArrayList<Movie> movies = new ArrayList<Movie>();
         try {
-            File myFile = new File("movies_data.csv");
-            Scanner input = new Scanner(myFile);
-            while (input.hasNext()) {
-                String data = myScanner.nextLine();
-                String[] dataSplit = data.split(",");
-                String title = dataSplit[0];
-                String cast = dataSplit[1];
-                String director = dataSplit[2];
-                String overview = dataSplit[3];
-                int runTime = Integer.parseInt(dataSplit[4]);
-                double rating = Double.parseDouble(dataSplit[5]);
-                Movie movie = new Movie(rating, title, director, overview, runTime, cast);
+            File myFile = new File("src//movie_data.csv");
+            Scanner fileScanner = new Scanner(myFile);
+            while (fileScanner.hasNext()) {
+                String data = fileScanner.nextLine();
+                String[] splitData = data.split(",");
+                String title=splitData[0];
+                String cast=splitData[1];
+                String director=splitData[2];
+                String overview=splitData[3];
+                int runtime=Integer.parseInt(splitData[4]);
+                double rating=Double.parseDouble(splitData[5]);
+                Movie movie = new Movie(title, cast, director, overview, runtime, rating);
                 movies.add(movie);
             }
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
+        } catch (IOException exception) {
+            System.out.println(exception.getMessage());
         }
-    }
-
-    public void searchTitle (String title) {
-        ArrayList<Movie> movies2 = new ArrayList<Movie>();
-        for (int i = 0; i < movies.size(); i++) {
-            movies2.set(i, movies.get(i));
-        }
-        System.out.println(movies2);
-    }
-
-
-    public void MovieRunner() {
         System.out.println("Welcome to the movie collection!");
         String menuOption = "";
-
+        Scanner scan = new Scanner(System.in);
         while (!menuOption.equals("q")) {
             System.out.println("------------ Main Menu ----------");
             System.out.println("- search (t)itles");
             System.out.println("- search (c)ast");
             System.out.println("- (q)uit");
             System.out.print("Enter choice: ");
-            menuOption = myScanner.nextLine();
+            menuOption = scan.nextLine();
 
             if (menuOption.equals("t")) {
-                movie.getTitle();
+                searchTitles(scan,movies);
             } else if (menuOption.equals("c")) {
-                movie.getCast();
+                searchCast(scan,movies);
             } else if (menuOption.equals("q")) {
                 System.out.println("Goodbye!");
-                return;
             } else {
                 System.out.println("Invalid choice!");
             }
         }
+    }
 
+
+    public void searchCast(Scanner scan, ArrayList<Movie> movies) {
+        System.out.println("Enter a person to search for (first or last): ");
+        String castName = scan.nextLine();
+        ArrayList<String> castNames = new ArrayList<>();
+        for (Movie movie : movies) {
+            for (int i = 0; i < movie.getCast().size(); i++) {
+                String currentName = movie.getCast().get(i);
+                if (currentName.contains(castName) && !castNames.contains(currentName)) {
+                    castNames.add(currentName);
+                }
+            }
+        }
+        for (int j = 1; j < castNames.size(); j++) {
+            String temp = castNames.get(j);
+            int possibleIndex = j;
+            while (possibleIndex > 0 && temp.compareTo(castNames.get(possibleIndex - 1)) < 0) {
+                castNames.set(possibleIndex, castNames.get(possibleIndex - 1));
+                possibleIndex--;
+            }
+            castNames.set(possibleIndex, temp);
+        }
+        if (castNames.size() == 0) {
+            System.out.println("No results match your search.");
+        } else {
+            for (int i = 0; i < castNames.size(); i++) {
+                System.out.println(i + 1 + ". " + castNames.get(i));
+            }
+
+            System.out.println("Which would you like to see all the movies for?");
+            System.out.println("Enter a number: ");
+            String currentName = castNames.get(scan.nextInt() - 1);
+            scan.nextLine();
+            ArrayList<Movie> moviesWithCast = new ArrayList<>();
+            for (int i = 0; i < movies.size(); i++) {
+                if (movies.get(i).getCast().contains(currentName)) {
+                    moviesWithCast.add(movies.get(i));
+                }
+            }
+            for (int j = 1; j < moviesWithCast.size(); j++) {
+                Movie temp = moviesWithCast.get(j);
+                int possibleIndex = j;
+                while (possibleIndex > 0 && temp.getTitle().compareTo(moviesWithCast.get(possibleIndex - 1).getTitle()) < 0) {
+                    moviesWithCast.set(possibleIndex, moviesWithCast.get(possibleIndex - 1));
+                    possibleIndex--;
+                }
+                moviesWithCast.set(possibleIndex, temp);
+            }
+            for (int i = 0; i < moviesWithCast.size(); i++) {
+                System.out.println(i + 1 + ". " + moviesWithCast.get(i).getTitle());
+            }
+            System.out.println("Which movie would you like to learn more about?");
+            System.out.println("Enter number: ");
+            Movie currentMovie = moviesWithCast.get(scan.nextInt() - 1);
+            scan.nextLine();
+            System.out.println(currentMovie);
+        }
+    }
+
+    public void searchTitles(Scanner scan, ArrayList<Movie>movies){
+        ArrayList<Movie>matches=new ArrayList<Movie>();
+        System.out.print("Enter a title search term: ");
+        String term = scan.nextLine();
+        for (Movie movie:movies) {
+            if (movie.getTitle().toLowerCase().contains(term.toLowerCase())) {
+                matches.add(movie);
+            }
+        }
+
+        for (int j = 1; j < matches.size(); j++) {
+            String temp = matches.get(j).getTitle();
+            Movie movie = matches.get(j);
+            int possibleIndex = j;
+            while (possibleIndex > 0 && temp.compareTo(matches.get(possibleIndex - 1).getTitle()) < 0) {
+                matches.set(possibleIndex, matches.get(possibleIndex - 1));
+                possibleIndex--;
+            }
+            matches.set(possibleIndex, movie);
+        }
+        if(matches.size()==0){
+            System.out.println("NO movies sorrryyyy");
+        } else {
+            int i=1;
+            for (Movie movie:matches) {
+                System.out.println(i+". "+movie.getTitle());
+                i++;
+            }
+            System.out.print("Pick a movie to learn about: ");
+            int movie_number = Integer.parseInt(scan.nextLine());
+            System.out.println(matches.get(movie_number));
+            System.out.println("Press enter to continue");
+            scan.nextLine();
+        }
+    }
+
+  /*public ArrayList<Movie>searchCasts(Scanner scan, ArrayList<Movie>movies){
+
+  }*/
+
+    public MovieCollection() {
+        start();
     }
 }
